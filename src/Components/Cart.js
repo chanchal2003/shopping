d8a7566f6011acc0b758432dee9cd7142d8b44d5
chanchal2003 <wadhwachanchal2003@gmail.com>
2024-02-12 22:11:26 +0530

    basic_page_structure

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -3,17 +3,112 @@ import React from "react";
 import "./cartnew.css"
 
 const Cart = () => {
-    return(
+    return (
         <>
             <header>
                 <div className="continue-shopping">
-                    <h2>Shopping Cart</h2>
+                    <img src="./images/arrow.png" alt="arrow" className="arrow-icon" />
+                    <h3>Continue Shopping</h3>
                 </div>
                 <div className="cart-icon">
-                    <img src="./images/cart.png" alt="cart" srcSet=""/>
+                    <img src="./images/cart.png" alt="cart" srcSet="" />
                     <p>7</p>
                 </div>
             </header>
+
+
+            <section className="main-cart-section">
+                <h1>Shopping Cart</h1>
+                <p className="total-items">
+                    you have<span className="total-items-count">4</span>items
+                    in shopping cart
+                </p>
+            </section>
+            {/* <Scrollbars></Scrollbars> */}
+
+            <section>
+                <div className="cart-items">
+                    <div className="cart-items-container">
+                        <div className="items-info">
+                            <div className="products-img">
+                                <img src="https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+                            </div>
+                        </div>
+
+                        <div className="title">
+                            <h2>Samsung 21</h2>
+                        </div>
+
+                        <div className="add-minus-quantity">
+                        <i class="fa-solid fa-minus"></i>
+                        <input type="text" placeholder="2"></input>
+                        <i class="fa-solid fa-plus"></i>
+                        </div>
+
+                        <div className="price">
+                            <h3>3000rs</h3>
+                        </div>
+
+                        <div className="remove-item">
+                        <i class="fa-solid fa-trash-alt"></i>
+
+                        </div>
+                    </div>
+                    <div className="cart-items-container">
+                        <div className="items-info">
+                            <div className="products-img">
+                                <img src="https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+                            </div>
+                        </div>
+
+                        <div className="title">
+                            <h2>Samsung 21</h2>
+                        </div>
+
+                        <div className="add-minus-quantity">
+                        <i class="fa-solid fa-minus"></i>
+                        <input type="text" placeholder="2"></input>
+                        <i class="fa-solid fa-plus"></i>
+                        </div>
+
+                        <div className="price">
+                            <h3>3000rs</h3>
+                        </div>
+
+                        <div className="remove-item">
+                        <i class="fa-solid fa-trash-alt"></i>
+
+                        </div>
+                    </div>
+                    <div className="cart-items-container">
+                        <div className="items-info">
+                            <div className="products-img">
+                                <img src="https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>
+                            </div>
+                        </div>
+
+                        <div className="title">
+                            <h2>Samsung 21</h2>
+                        </div>
+
+                        <div className="add-minus-quantity">
+                        <i class="fa-solid fa-minus"></i>
+                        <input type="text" placeholder="2"></input>
+                        <i class="fa-solid fa-plus"></i>
+                        </div>
+
+                        <div className="price">
+                            <h3>3000rs</h3>
+                        </div>
+
+                        <div className="remove-item">
+                        <i class="fa-solid fa-trash-alt"></i>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
         </>
     )
 }
